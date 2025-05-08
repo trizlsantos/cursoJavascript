eb2ccf6d5f0dfc1a95616c25346f86e31ea8e1b1
bia <beatriz.l.santos7@aluno.senai.br>
2025-05-08 16:55:20 -0300

    Add files via upload

---
+++ Aula02/aula02.js
@@ -0,0 +1,19 @@
+let nome;
+let idade;
+let altura;
+let programador;
+
+nome = prompt("Seu nome: ")
+idade = prompt("Sua idade: ")
+altura = prompt("Sua altura: ")
+programador = prompt("É programador: ")
+
+console.log("Nome: ", nome);
+console.log("Idade: ", idade);
+console.log("Altura: ", altura);
+console.log("Programador: ", programador);
+
+//Tipos de Dados
+console.log('Nome: ', typeof(nome));
+console.log('Idade: ', typeof(idade));
+
---
+++ Aula02/exercicios.js
@@ -0,0 +1,33 @@
+let nome = prompt("Qual é o seu nome?");
+let idade = prompt("Qual é a sua idade?");
+let altura = prompt("Qual a sua altura?");
+let genero = prompt("Qual é o seu gênero?");
+let estudante = prompt("Você é estudante?");
+
+console.log(`bem vindo, ${nome} !`)
+console.log(`seu perfil: `)
+console.log("nome: ", nome);
+console.log("idade: ", idade);
+console.log("altura: ", altura);
+console.log("genero: ", genero);
+console.log("estudante: ", estudante);
+
+// Exercicio 02
+
+//operador ternario
+// condicao ? expressao1 : expressao2;
+
+let valorCompra = parseInt("Informe o valor da compra");
+let desconto; 
+let valorFinal;
+
+valorCompra > 100 ? desconto 
+
+
+
+
+
+
+
+
+
